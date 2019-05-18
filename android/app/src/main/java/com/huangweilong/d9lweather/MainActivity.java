4e2cc0fa9686f3c3d5ef269eedb46bfb33898e04
huang-weilong <550456817@qq.com>
2019-05-18 17:04:20 +0800

    init

---
+++ android/app/src/main/java/com/huangweilong/d9lweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huangweilong.d9lweather;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'd9l weather',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('welcome to d9l weather'),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Center(child: Text('d9l天气APP')),
+    );
+  }
+}
