7112ed64f3e1033592c528264a0260b3511f22b2
huang-weilong <550456817@qq.com>
2020-03-06 18:10:05 +0800

    three-day date

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization_delegate.dart';
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
@@ -5,7 +6,7 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('关于'),
+        title: Text(AppLocalizations.of(context).tr('about')),
         centerTitle: true,
         elevation: 0.0,
         backgroundColor: Colors.blue.withOpacity(0.9),
